1075cd8dc2dfa6b889d3c9d08e22094966f64895
Anand <anandpillai@gmail.com>
2025-06-03 12:36:01 -0700

    Enable the extension only on cash-back deals page.
    Use the Chrome declarativeContent API to keep the
    extension icon disabled until the US Bank
    cash-back deals page containing unactivated offers
    is loaded.
    
    Replaced host_permissions with activeTab.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # U.S. Bank Card cash-back deals activator
 
-This Chrome extension activates all cash-back deals on the U.S Bank website.
+This Chrome extension activates all cash-back deals on the U.S. Bank website.
 
 ## Usage
 
 1. Navigate to the U.S. Bank cash-back deals web page.
 2. Click the extension icon to activate all the cash-back deals on the page.
+
+> [!NOTE]
+> The extension icon will remain disabled until the U.S. Bank cash-back
+> deals page containing unactivated offers is opened.
--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+// When the extension icon is clicked, automatically click all the
+// activate buttons on the page.
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
@@ -6,5 +8,27 @@ chrome.action.onClicked.addListener((tab) => {
           .querySelectorAll('.unActiveOffer')
           .forEach(el => el.click());
     }
+  });
+});
+
+// Keep the extension icon disabled until the tab contains the US Bank page
+// with the cash-back deal activate buttons.
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    let enablingRule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostSuffix: '.usbank.com' },
+          css: ['.unActiveOffer']
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { schemes: ['file'] },
+          css: ['.unActiveOffer']
+        }),
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()]
+    };
+    chrome.declarativeContent.onPageChanged.addRules([enablingRule]);
   });
 });
